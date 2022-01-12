53a3548e4dd2d3d2f1a1ae827c521b116ca40d11
Xanonymous <trusaidlin@gmail.com>
2022-01-12 08:27:38 +0800

    amount

--- src/pages/magics/[productId].vue
+++ src/pages/magics/[productId].vue
@@ -7,20 +7,20 @@
           <img class="h-full w-full rounded-md object-cover max-w-lg mx-auto" :src="magic.pictureUrl" alt="">
         </div>
         <div class="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
-          <h3 class="text-gray-700 uppercase text-lg">
+          <h3 class="text-gray-700 uppercase text-2xl">
             {{ magic.name }}
           </h3>
-          <span class="text-gray-500 mt-3">${{ magic.price }}</span>
+          <span class="text-gray-500 mt-3 text-5xl">${{ totalPrice.toString().replace(NaN.toString(), '') }}</span>
           <hr class="my-3">
           <p>{{ magic.description }}</p>
           <div class="mt-2">
             <label class="text-gray-700 text-sm">Count:</label>
             <div class="flex items-center mt-1">
-              <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <button class="text-gray-500 focus:outline-none focus:text-gray-600" @click.prevent="amount++">
                 <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
               </button>
-              <span class="text-gray-700 text-lg mx-2">20</span>
-              <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <span class="text-gray-700 text-lg mx-2">{{amount}}</span>
+              <button class="text-gray-500 focus:outline-none focus:text-gray-600" @click.prevent="() => {if (amount > 1)amount--}">
                 <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
               </button>
             </div>
@@ -28,11 +28,11 @@
 
           <div class="flex items-center mt-6">
             <button class="px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
-              Buy Now
+              Buy
             </button>
             <router-link to="/magics" class="mx-4">
               <button class="px-8 py-2 bg-green-600 text-white text-sm font-medium rounded hover:bg-green-500 focus:outline-none focus:bg-green-500">
-                Back to Magics
+                Back
               </button>
             </router-link>
             <button class="mx-2 text-gray-600 border rounded-md p-2 hover:bg-gray-200 focus:outline-none">
@@ -55,6 +55,11 @@ const route = useRoute()
 const magicId = route.path?.replace('/magics/', '')
 
 const magic = ref<Magic>({} as Magic)
+const amount = ref<number>(1)
+const totalPrice = computed(() => {
+  const real = magic.value.price * amount.value
+  return amount.value <= 10 ? real : Math.ceil(real * (1 - Math.min(amount.value * 0.01, 0.5)))
+})
 
 tryOnMounted(async() => {
   magic.value = await getSpecificMagic(magicId)
